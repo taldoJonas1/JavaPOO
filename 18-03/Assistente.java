6d8503b3a649a65dff1732c60ee8fb50f890863b
João Pucci <126732888+taldoJonas1@users.noreply.github.com>
2024-03-18 22:15:07 -0300

    Create Assistente.java

---
+++ 18-03/Assistente.java
@@ -0,0 +1,32 @@
+public class Assistente extends Funcionario{
+    private float horaExtra;
+    public Assistente(){
+        super(); // chama construtor da classe Pai
+    }
+
+    public Assistente(String nome, String endereco, String cpf,
+                      float salario, float horaExtra) {
+        // chama construtor da classe Pai - reusabilidade de código-fonte
+        super(nome, endereco, cpf, salario);
+        this.horaExtra = horaExtra;
+    }
+
+    public float getHoraExtra() {
+        return horaExtra;
+    }
+    public void setHoraExtra(float horaExtra) {
+        this.horaExtra = horaExtra;
+    }
+
+    @Override
+    public String toString() {
+        return "Assistente{" +
+                super.toString() + "horaExtra=" + horaExtra +
+                '}';
+    }
+
+    @Override
+    public float calculaSalario(){
+        return super.calculaSalario() + this.horaExtra * 50;
+    }
+}
